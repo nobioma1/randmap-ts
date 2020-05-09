dca36aa0438ececeddf9519534a063fd11a1f5fd
Noble Obioma <nobioma1@gmail.com>
2020-05-09 19:55:46 +0100

    Initialize User, Company, CustomMap class and a marker

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hi there!');
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+import { User } from './User';
+
+const user = new User();
+const company = new Company();
+
+const map = new CustomMap('map');
+
+map.addMarker(user);
+map.addMarker(company);
